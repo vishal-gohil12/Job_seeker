33ee064fb32128e2389dff18d84f437c385224fb
vishal-gohil12 <21it039@charusat.edu.in>
2024-06-15 21:23:04 +0530

    first

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ export const app = express();
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with your frontend URL
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
